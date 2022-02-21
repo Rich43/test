532fbc506b59d192167cbb030bda2c25e9e191d5
Richard Ward <RichieS@GMail.com>
2022-02-21 20:18:50 +0000

    Add stub tests

---
+++ test/src/test/java/com/example/test/controller/CustomerControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.test.controller;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerControllerTest {
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}
---
+++ test/src/test/java/com/example/test/service/CustomerServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.test.service;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerServiceTest {
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void createCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}
